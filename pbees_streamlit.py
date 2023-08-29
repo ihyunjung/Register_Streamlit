dad12273b17699053687ce8d5637917a6a56b20e
ihyunjung <109563978+ihyunjung@users.noreply.github.com>
2023-08-29 16:42:20 +0900

    Update pbees_streamlit.py

--- pbees_streamlit.py
+++ pbees_streamlit.py
@@ -15,7 +15,7 @@ st.markdown("<p style='text-align: center; color: white;'>정보를 입력해주
 
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
-    my_cur.execute("insert into fruit_load_list values('"+new_fruit+"')")
+    my_cur.execute("insert into customer values('"+new_fruit+"')")
     return "Thanks for adding " + new_fruit
 
 
