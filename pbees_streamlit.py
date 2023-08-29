ed181407be3e07c7a5f4197f8da421fce11f0c10
ihyunjung <109563978+ihyunjung@users.noreply.github.com>
2023-08-29 15:47:48 +0900

    Update pbees_streamlit.py

--- pbees_streamlit.py
+++ pbees_streamlit.py
@@ -1,5 +1,7 @@
 import streamlit as st
+from PIL import Image
 
-st.image('C:/Users/PBees/Documents/hyunjung/Snowflake/0905 헹시준비/Pbees Character.png')
+
 st.markdown("<h1 style='text-align: center; color: white;'>P.Bees 회원가입</h1>", unsafe_allow_html=True)
 st.markdown("<p style='text-align: center; color: white;'>정보를 입력해주세요</p>", unsafe_allow_html=True)
+Pbees_character = Image.open('Pbees_Character.png')
