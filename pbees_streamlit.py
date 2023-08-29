439dc952c74da8a79fc0df6b2392684bf0b6db11
ihyunjung <109563978+ihyunjung@users.noreply.github.com>
2023-08-29 15:26:07 +0900

    Update pbees_streamlit.py

--- pbees_streamlit.py
+++ pbees_streamlit.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.markdown("<h1 style='text-align: center; color: white;'>P.Bees 회원가입</h1>", unsafe_allow_html=True)
-st.header('정보를 입력해주세요')
+st.markdown("<p style='text-align: center; color: white;'>정보를 입력해주세요</p>", unsafe_allow_html=True)
