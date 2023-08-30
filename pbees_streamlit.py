b4bc97a6f6659a63a7cb27b9798bf49c7fee54b0
ihyunjung <109563978+ihyunjung@users.noreply.github.com>
2023-08-30 13:02:13 +0900

    Update pbees_streamlit.py

--- pbees_streamlit.py
+++ pbees_streamlit.py
@@ -10,7 +10,18 @@ def insert_row_snowflake(name, phone, email, company):
     query = "insert into customer (NAME, PHONE, EMAIL, COMPANY) values('{name}', '{phone}', '{email}', '{company}');".format(name = name, phone = phone, email = email, company = company)
     my_cur.execute(query)
     return "Thanks for adding"
+# 
+html_string = """
+            <audio controls autoplay>
+              <source src="https://www.orangefreesounds.com/wp-content/uploads/2022/04/Small-bell-ringing-short-sound-effect.mp3" type="audio/mp3">
+            </audio>
+            """
 
+sound = st.empty()
+sound.markdown(html_string, unsafe_allow_html=True)  # will display a st.audio with the sound you specified in the "src" of the html_string and autoplay it
+time.sleep(2)  # wait for 2 seconds to finish the playing of the audio
+sound.empty()  # optionally delete the element afterwards
+# 
 
 add_my_name = st.text_input('이름')
 add_my_phone = st.text_input('핸드폰 번호 ( - 는 제외해주세요)')
