6c0335cb54303af932314644fa415768c0fc0114
ihyunjung <109563978+ihyunjung@users.noreply.github.com>
2023-08-30 14:06:00 +0900

    Update pbees_streamlit.py

--- pbees_streamlit.py
+++ pbees_streamlit.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import snowflake.connector
 
-st.markdown("<h1 style='text-align: center; color: black;'>송이송이 눈꽃송이</h1>", unsafe_allow_html=True)
-st.markdown("<p style='text-align: center; color: black;'>Snowflake 기능 체험하기</p>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>송이송이 눈꽃송이</h1>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: center;'>Snowflake 기능 체험하기</p>", unsafe_allow_html=True)
 
 
 def insert_row_snowflake(name, phone, email, company):
