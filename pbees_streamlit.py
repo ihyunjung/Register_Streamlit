bc68a5703dc89c5e080b3beb6b26eb0008857534
ihyunjung <109563978+ihyunjung@users.noreply.github.com>
2023-08-30 13:46:13 +0900

    Update pbees_streamlit.py

--- pbees_streamlit.py
+++ pbees_streamlit.py
@@ -10,18 +10,7 @@ def insert_row_snowflake(name, phone, email, company):
     query = "insert into customer (NAME, PHONE, EMAIL, COMPANY) values('{name}', '{phone}', '{email}', '{company}');".format(name = name, phone = phone, email = email, company = company)
     my_cur.execute(query)
     return "Thanks for adding"
-# 
-# html_string = """
-#             <audio controls autoplay>
-#               <source src="https://www.orangefreesounds.com/wp-content/uploads/2022/04/Small-bell-ringing-short-sound-effect.mp3" type="audio/mp3">
-#             </audio>
-#             """
 
-# sound = st.empty()
-# sound.markdown(html_string, unsafe_allow_html=True)  # will display a st.audio with the sound you specified in the "src" of the html_string and autoplay it
-# time.sleep(2)  # wait for 2 seconds to finish the playing of the audio
-# sound.empty()  # optionally delete the element afterwards
-# 
 
 add_my_name = st.text_input('이름')
 add_my_phone = st.text_input('핸드폰 번호 ( - 는 제외해주세요)')
@@ -32,7 +21,7 @@ add_my_company = st.text_input('소속')
 
 
 col1, col2, col3, col4, col5 = st.columns(5)
-if col3.button('응모완료'):
+if col3.button('체험하기'):
   if not add_my_name:
     st.error('이름을 입력해주세요')
   elif not add_my_phone:
